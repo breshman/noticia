ed5c61084721c7f3841ed1403d5083f233350b2b
breshman <breshman12@gmail.com>
2019-12-04 21:29:40 +0000

    se quito las lines de item

--- src/app/paginas/tab1/tab1.page.ts
+++ src/app/paginas/tab1/tab1.page.ts
@@ -1,12 +1,71 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+    selector: 'app-tab1',
+    templateUrl: 'tab1.page.html',
+    styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
 
-  constructor() {}
+    lista = [
+        {
+            name: 'MENDIGURE CONDO BEATRIZ',
+            direccion: 'AV. MANCO CCAPAC NRO. 439 (EL PAISANO) - LIMA - LIMA - BREÑA',
+            verdedor: 'Nombre de vendedor',
+            estado: 'Entregado',
+            fechaEntrega: ''
+        },
+        {
+            name: 'MENDIGURE CONDO BEATRIZ',
+            direccion: 'AV. MANCO CCAPAC NRO. 439 (EL PAISANO) - LIMA - LIMA - BREÑA',
+            verdedor: 'Nombre de vendedor',
+            estado: 'En ruta',
+            fechaEntrega: ''
+        },
+        {
+            name: 'MENDIGURE CONDO BEATRIZ',
+            direccion: 'AV. MANCO CCAPAC NRO. 439 (EL PAISANO) - LIMA - LIMA - BREÑA',
+            verdedor: 'Nombre de vendedor',
+            estado: 'En ruta',
+            fechaEntrega: ''
+        },
+        {
+            name: 'MENDIGURE CONDO BEATRIZ',
+            direccion: 'AV. MANCO CCAPAC NRO. 439 (EL PAISANO) - LIMA - LIMA - BREÑA',
+            verdedor: 'Nombre de vendedor',
+            estado: 'Pendiente',
+            fechaEntrega: ''
+        },
+        {
+            name: 'MENDIGURE CONDO BEATRIZ',
+            direccion: 'AV. MANCO CCAPAC NRO. 439 (EL PAISANO) - LIMA - LIMA - BREÑA',
+            verdedor: 'Nombre de vendedor',
+            estado: 'Pendiente',
+            fechaEntrega: ''
+        },
+        {
+            name: 'MENDIGURE CONDO BEATRIZ',
+            direccion: 'AV. MANCO CCAPAC NRO. 439 (EL PAISANO) - LIMA - LIMA - BREÑA',
+            verdedor: 'Nombre de vendedor',
+            estado: 'Pendiente',
+            fechaEntrega: ''
+        },
+        {
+            name: 'MENDIGURE CONDO BEATRIZ',
+            direccion: 'AV. MANCO CCAPAC NRO. 439 (EL PAISANO) - LIMA - LIMA - BREÑA',
+            verdedor: 'Nombre de vendedor',
+            estado: 'Pendiente',
+            fechaEntrega: ''
+        },
+        {
+            name: 'MENDIGURE CONDO BEATRIZ',
+            direccion: 'AV. MANCO CCAPAC NRO. 439 (EL PAISANO) - LIMA - LIMA - BREÑA',
+            verdedor: 'Nombre de vendedor',
+            estado: 'Pendiente',
+            fechaEntrega: ''
+        },
+    ]
+
+    constructor() { }
 
 }
