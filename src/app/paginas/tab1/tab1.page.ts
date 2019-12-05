9413bbd05707c78b108e776d2996e1e93e307ee6
breshman <breshman12@gmail.com>
2019-12-05 20:38:59 +0000

    as

--- src/app/paginas/tab1/tab1.page.ts
+++ src/app/paginas/tab1/tab1.page.ts
@@ -13,56 +13,174 @@ export class Tab1Page {
             direccion: 'AV. MANCO CCAPAC NRO. 439 (EL PAISANO) - LIMA - LIMA - BREÑA',
             verdedor: 'Nombre de vendedor',
             estado: 'Entregado',
-            fechaEntrega: ''
+            fechaEntrega: '',
+            documentos: [
+                {
+                   numeroGR: '001-0020139',
+                    numeroFT: 'F009-00000108', 
+                },
+                {
+                   numeroGR: '001-0020139',
+                    numeroFT: 'F009-00000108', 
+                },
+                {
+                   numeroGR: '001-0020139',
+                    numeroFT: 'F009-00000108', 
+                },
+            ],
+            numeroGR: '001-0020139',
+            numeroFT: 'F009-00000108',
+        },
+        {
+            name: 'MENDIGURE CONDO BEATRIZ',
+            direccion: 'AV. MANCO CCAPAC NRO. 439 (EL PAISANO) - LIMA - LIMA - BREÑA',
+            verdedor: 'Nombre de vendedor',
+            estado: 'Entregado',
+            fechaEntrega: '',
+            documentos: [
+                {
+                   numeroGR: '001-0020139',
+                    numeroFT: 'F009-00000108', 
+                },
+                {
+                   numeroGR: '001-0020139',
+                    numeroFT: 'F009-00000108', 
+                },
+                {
+                   numeroGR: '001-0020139',
+                    numeroFT: 'F009-00000108', 
+                },
+            ],
+            numeroGR: '001-0020139',
+            numeroFT: 'F009-00000108',
         },
         {
             name: 'MENDIGURE CONDO BEATRIZ',
             direccion: 'AV. MANCO CCAPAC NRO. 439 (EL PAISANO) - LIMA - LIMA - BREÑA',
             verdedor: 'Nombre de vendedor',
             estado: 'En ruta',
-            fechaEntrega: ''
-        },
-        {
-            name: 'MENDIGURE CONDO BEATRIZ',
-            direccion: 'AV. MANCO CCAPAC NRO. 439 (EL PAISANO) - LIMA - LIMA - BREÑA',
-            verdedor: 'Nombre de vendedor',
-            estado: 'En ruta',
-            fechaEntrega: ''
-        },
-        {
-            name: 'MENDIGURE CONDO BEATRIZ',
-            direccion: 'AV. MANCO CCAPAC NRO. 439 (EL PAISANO) - LIMA - LIMA - BREÑA',
-            verdedor: 'Nombre de vendedor',
-            estado: 'Pendiente',
-            fechaEntrega: ''
-        },
-        {
-            name: 'MENDIGURE CONDO BEATRIZ',
-            direccion: 'AV. MANCO CCAPAC NRO. 439 (EL PAISANO) - LIMA - LIMA - BREÑA',
-            verdedor: 'Nombre de vendedor',
-            estado: 'Pendiente',
-            fechaEntrega: ''
-        },
-        {
-            name: 'MENDIGURE CONDO BEATRIZ',
-            direccion: 'AV. MANCO CCAPAC NRO. 439 (EL PAISANO) - LIMA - LIMA - BREÑA',
-            verdedor: 'Nombre de vendedor',
-            estado: 'Pendiente',
-            fechaEntrega: ''
+            fechaEntrega: '',
+            numeroGR: '001-0024564',
+            numeroFT: 'F009-0006654',
+            documentos: [
+                {
+                   numeroGR: '001-0020139',
+                    numeroFT: 'F009-00000108', 
+                },
+                {
+                   numeroGR: '001-0020139',
+                    numeroFT: 'F009-00000108', 
+                },
+                {
+                   numeroGR: '001-0020139',
+                    numeroFT: 'F009-00000108', 
+                },
+            ],
         },
         {
             name: 'MENDIGURE CONDO BEATRIZ',
             direccion: 'AV. MANCO CCAPAC NRO. 439 (EL PAISANO) - LIMA - LIMA - BREÑA',
             verdedor: 'Nombre de vendedor',
             estado: 'Pendiente',
-            fechaEntrega: ''
+            fechaEntrega: '',
+            numeroGR: '001-0020139',
+            numeroFT: 'F009-00000525',
+            documentos: [
+                {
+                   numeroGR: '001-0020139',
+                    numeroFT: 'F009-00000108', 
+                },
+                {
+                   numeroGR: '001-0020139',
+                    numeroFT: 'F009-00000108', 
+                },
+                {
+                   numeroGR: '001-0020139',
+                    numeroFT: 'F009-00000108', 
+                },
+            ],
         },
         {
             name: 'MENDIGURE CONDO BEATRIZ',
             direccion: 'AV. MANCO CCAPAC NRO. 439 (EL PAISANO) - LIMA - LIMA - BREÑA',
             verdedor: 'Nombre de vendedor',
             estado: 'Pendiente',
-            fechaEntrega: ''
+            fechaEntrega: '',
+            numeroGR: '001-002065',
+            numeroFT: 'F009-00000109',
+            documentos: [
+                {
+                   numeroGR: '001-0020139',
+                    numeroFT: 'F009-00000108', 
+                },
+                {
+                   numeroGR: '001-0020139',
+                    numeroFT: 'F009-00000108', 
+                },
+                {
+                   numeroGR: '001-0020139',
+                    numeroFT: 'F009-00000108', 
+                },
+            ],
+        },
+        {
+            name: 'MENDIGURE CONDO BEATRIZ',
+            direccion: 'AV. MANCO CCAPAC NRO. 439 (EL PAISANO) - LIMA - LIMA - BREÑA',
+            verdedor: 'Nombre de vendedor',
+            estado: 'Pendiente',
+            fechaEntrega: '',
+            numeroGR: '001-0020552',
+            numeroFT: 'F009-0005148',
+            documentos: [
+                {
+                   numeroGR: '001-0020139',
+                    numeroFT: 'F009-00000108', 
+                },
+              
+            ],
+        },
+        {
+            name: 'MENDIGURE CONDO BEATRIZ',
+            direccion: 'AV. MANCO CCAPAC NRO. 439 (EL PAISANO) - LIMA - LIMA - BREÑA',
+            verdedor: 'Nombre de vendedor',
+            estado: 'Pendiente',
+            fechaEntrega: '',
+            numeroGR: '001-0020139',
+            numeroFT: 'F009-00000154',
+            documentos: [
+                {
+                   numeroGR: '001-0020139',
+                    numeroFT: 'F009-00000108', 
+                },
+                {
+                   numeroGR: '001-0020139',
+                    numeroFT: 'F009-00000108', 
+                },
+                {
+                   numeroGR: '001-0020139',
+                    numeroFT: 'F009-00000108', 
+                },
+            ],
+        },
+        {
+            name: 'MENDIGURE CONDO BEATRIZ',
+            direccion: 'AV. MANCO CCAPAC NRO. 439 (EL PAISANO) - LIMA - LIMA - BREÑA',
+            verdedor: 'Nombre de vendedor',
+            estado: 'Pendiente',
+            fechaEntrega: '',
+            numeroGR: '001-0020185',
+            numeroFT: 'F009-0000335',
+            documentos: [
+                {
+                   numeroGR: '001-0020139',
+                    numeroFT: 'F009-00000108', 
+                },
+                {
+                   numeroGR: '001-0020139',
+                    numeroFT: 'F009-00000108', 
+                },
+               
+            ],
         },
     ]
 
