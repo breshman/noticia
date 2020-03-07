ad206db25fb1c54edfe81909d76b05f56259485f
breshman <breshman12@gmail.com>
2020-03-07 15:32:46 +0000

    add alert controler

--- src/app/paginas/tab3/tab3.page.ts
+++ src/app/paginas/tab3/tab3.page.ts
@@ -1,12 +1,51 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
-  selector: 'app-tab3',
-  templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss']
+    selector: 'app-tab3',
+    templateUrl: 'tab3.page.html',
+    styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
 
-  constructor() {}
+    cantidad:number;
+    data;
+    constructor(
+        private alertCtl: AlertController
+    ) { }
 
+    async presentAlert() {
+        const alert = await this.alertCtl.create({
+            header: 'Alert',
+            // subHeader: 'Subtitle',
+            message: 'Message <strong>text</strong>!!!',
+            inputs: [
+                {
+                    name: 'cantidad',
+                    type: 'number',
+                    placeholder: 'Sleciones las cantidad',
+                    value: this.cantidad | 0
+                },
+            ],
+            buttons: [
+                {
+                    text: 'Cancelar',
+                    role: 'cancel',
+                    cssClass: 'secondary',
+                    handler: (blah) => {
+                       this.data = blah
+                    }
+                }, {
+                    text: 'Confirmar',
+                    handler: (n) => {
+                        this.cantidad = n.cantidad
+                        this.data = n
+                    },
+                    
+                }
+            ]
+        });
+
+        await alert.present();
+    }
 }
